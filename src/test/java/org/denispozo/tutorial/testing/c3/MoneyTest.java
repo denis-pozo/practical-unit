c642d5f86c06f8716b1d5c209b7c3360ac1664f1
Denis Pozo <denispzpd@gmail.com>
2020-02-06 17:43:59 +0100

    Add parametrized test to Money

--- src/test/java/org/denispozo/tutorial/testing/c3/MoneyTest.java
+++ src/test/java/org/denispozo/tutorial/testing/c3/MoneyTest.java
@@ -3,9 +3,20 @@ package org.denispozo.tutorial.testing.c3;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class MoneyTest {
+
+    private static final Object[] getMoney() {
+        return new Object[] {
+            new Object[] {10, "USD"},
+            new Object[] {20, "EUR"}
+        };
+    }
 
     @Test
     public void constructorShouldSetAmountAndCurrency() {
@@ -13,6 +24,16 @@ public class MoneyTest {
 
         assertEquals(10, money.getAmount());
         assertEquals("USD", money.getCurrency());
+    }
+
+    @Test
+    @Parameters(method = "getMoney")
+    public void constructorShouldSetAmountAndCurrencyParametrized(
+        int amount, String currency) {
+        Money money = new Money(amount, currency);
+
+        assertEquals(amount, money.getAmount());
+        assertEquals(currency, money.getCurrency());
     }
 
     @Test
