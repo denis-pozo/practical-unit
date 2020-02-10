4ee6f60a4a32567f99da8e04217737f27af5bec4
Denis Pozo <denispzpd@gmail.com>
2020-02-10 21:05:54 +0100

    Finish example FootballTeam class and tests

--- src/main/java/org/denispozo/tutorial/testing/c4/example/FootballTeam.java
+++ src/main/java/org/denispozo/tutorial/testing/c4/example/FootballTeam.java
@@ -1,15 +1,23 @@
 package org.denispozo.tutorial.testing.c4.example;
 
-public class FootballTeam {
+public class FootballTeam implements Comparable<FootballTeam> {
 
     private int gamesWon = 0;
 
     public FootballTeam(int gamesWon) {
-        this.gamesWon = gamesWon;
+        if(gamesWon < 0) {
+            throw new IllegalArgumentException();
+        }
 
+        this.gamesWon = gamesWon;
     }
 
     public int getGamesWon() {
         return gamesWon;
+    }
+
+    @Override
+    public int compareTo(FootballTeam otherTeam) {
+        return gamesWon - otherTeam.getGamesWon();
     }
 }
--- src/test/java/org/denispozo/tutorial/testing/c4/example/FootballTeamTest.java
+++ src/test/java/org/denispozo/tutorial/testing/c4/example/FootballTeamTest.java
@@ -1,19 +1,84 @@
 package org.denispozo.tutorial.testing.c4.example;
 
+import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class FootballTeamTest {
 
-    private static final int THREE_GAMES_WON = 3;
+    private static final int ANY_NUMBER = 123;
+
+    public Object[] nbOfWins() {
+        return $(0, 1, 2, 3, 4);
+    }
+
+    public Object[] invalidNbOfWins() {
+        return $(-4, -100, -2, -1);
+    }
 
     @Test
-    public void constructorShouldSetGamesWon() {
-        FootballTeam team = new FootballTeam(THREE_GAMES_WON);
+    @Parameters(method = "nbOfWins")
+    public void constructorShouldSetGamesWonParam(int nbOfWins) {
+        FootballTeam team = new FootballTeam(nbOfWins);
 
-        assertEquals(THREE_GAMES_WON + " games passed to the constructor, but "
+        assertEquals(nbOfWins + " games passed to the constructor, but "
                      + team.getGamesWon() + " were returned",
-                     THREE_GAMES_WON, team.getGamesWon());
+                     nbOfWins, team.getGamesWon());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters(method = "invalidNbOfWins")
+    public void constructorShouldThrowIAEforInvalidNbOfWins(int invalidNbWins) {
+        FootballTeam team = new FootballTeam(invalidNbWins);
+        System.out.println("This line should never get printed. Nb of wins: " + team.getGamesWon());
+    }
+
+    @Test
+    public void souldBePossibleToCompareTeams() {
+        FootballTeam team = new FootballTeam(ANY_NUMBER);
+
+        assertTrue("FootballTeam should implement Comparable", team instanceof Comparable);
+    }
+
+    @Test
+    public void teamWithMoreWinsShouldBeGreater() {
+        int threeWins = 3;
+        int twoWins = 0;
+
+        FootballTeam first = new FootballTeam(threeWins);
+        FootballTeam second = new FootballTeam(twoWins);
+
+        assertTrue("team with " + first.getGamesWon() + " wins should be ranked before "
+                    + "the team with " + second.getGamesWon() + " wins",
+                   first.compareTo(second) > 0);
+    }
+
+    @Test
+    public void teamWithLessWinsShouldBeSmaller() {
+        int threeWins = 3;
+        int twoWins = 0;
+
+        FootballTeam first = new FootballTeam(threeWins);
+        FootballTeam second = new FootballTeam(twoWins);
+
+        assertTrue("team with " + first.getGamesWon() + " wins should be ranked before "
+                   + "the team with " + second.getGamesWon() + " wins",
+                   second.compareTo(first) < 0);
+    }
+
+    @Test
+    public void teamWithSameNumberOfWinsShouldBeEqual() {
+        int wins = 6;
+        FootballTeam teamA = new FootballTeam(wins);
+        FootballTeam teamB = new FootballTeam(wins);
+
+        assertTrue( "teams with same number of wins " + wins + " wins]"
+                    + " should be equal", teamA.compareTo(teamB) == 0);
     }
 }
